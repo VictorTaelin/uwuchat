eea44c7a50d2a778b73b4bc8fbbcdd42de827e92
Yan Mendes <53550620+kings177@users.noreply.github.com>
2023-10-25 20:08:34 -0300

    Update client.js

--- example/client.js
+++ example/client.js
@@ -1,5 +1,5 @@
 import client from "./../client.js";
-var api = client({url: "ws://localhost:7171"});
+var api = client({url: "wss://server.uwu.games"});
 
 // When connected, watches room 0 and makes an example post.
 api.on_init(() => {
